2c226abb4a014248a8e1283c4c94b1a7fef1aa7b
RomanDonw <romandonw628@gmail.com>
2026-07-04 20:48:33 +1000

    upgraded the second test (1.c)

--- testmodules/1.c
+++ testmodules/1.c
@@ -4,37 +4,43 @@
 #include <string.h>
 #include <getopt.h>
 
+static unsigned short ioportpairs = 0;
 static float modifier = 0;
 
 unsigned short dspmodule_startup(const char **name, unsigned short *inportscount, unsigned short *outportscount, int argc, char * const argv[])
 {
     {
         int p;
-        while ((p = getopt(argc, argv, "m:")) != -1)
+        while ((p = getopt(argc, argv, "m:p:")) != -1)
         {
             switch (p)
             {
                 case 'm':
                     if (sscanf(optarg, "%f", &modifier) < 1) { puts("error parsing option -m"); return 1; }
                     break;
+
+                case 'p':
+                    if (sscanf(optarg, "%hu", &ioportpairs) < 1) { puts("error parsing option -p"); return 1; }
+                    break;
             }
         }
     }
 
-    printf("modifier: %f\n", modifier);
+    if (!ioportpairs) { puts("specify at least one I/O ports pair through -p parameter"); return 1; }
+    printf("I/O ports pairs: %hu\nmodifier: %f\n", ioportpairs, modifier);
 
     *name = "amplitude modifier";
-    *inportscount = 2;
-    *outportscount = 2;
+    *inportscount = ioportpairs;
+    *outportscount = ioportpairs;
     return 0;
 }
 
 unsigned short dspmodule_process(const float * const inbuffers[], float * const outbuffers[], unsigned long samplescount)
 {
-    for (unsigned char j = 0; j < 2; j++)
+    for (unsigned char ch = 0; ch < ioportpairs; ch++)
     {
-        const float *in = inbuffers[j];
-        float *out = outbuffers[j];
+        const float *in = inbuffers[ch];
+        float *out = outbuffers[ch];
 
         if (!out) continue;
         if (!in) { memset(out, 0, sizeof(float) * samplescount); continue; }
